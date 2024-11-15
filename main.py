f0d120fd59aa121e3e617ecae4d0dac6a4991b0c
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2024-11-15 15:47:20 -0500

    first push

--- main.py
+++ main.py
@@ -1,1 +1,48 @@
-print('hello')
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask, request, render_template, redirect, url_for, flash, abort, send_from_directory
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve database credentials from environment variables
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+
+try:
+    # Test database connection
+    connection = psycopg2.connect(
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=db_port
+    )
+    print("Connection to the database was successful!")
+
+except Exception as e:
+    print("Failed to connect to the database.")
+    print(f"Error: {e}")
+
+finally:
+    if 'connection' in locals() and connection:
+        connection.close()
+        print("Connection closed.")
+
+app = Flask(__name__)
+@app.route("/courses")
+def courses():
+    try:
+        # Query the database to retrieve student data
+        #result = db.session.execute(text("SELECT * FROM users"))
+        #students = result.fetchall()  # Fetch all rows
+
+        return render_template("courses.html")
+
+    except Exception as e:
+        print(f"Error fetching student data: {e}")
+        abort(500, description="Error fetching data from the database.")
