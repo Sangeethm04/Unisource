2939e8f1a00d2065aa06560ae39420d60eccd662
Sangeeth Menachery <43912785+Sangeethm04@users.noreply.github.com>
2024-11-22 20:44:27 -0500

    working db

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from flask import Flask, request, render_template, redirect, url_for, flash, abo
 # Load environment variables from .env file
 load_dotenv()
 
+
 # Retrieve database credentials from environment variables
-db_name = "unisourcedb"
-db_user = "unisourcedbuser"
-db_password = "deuce-unpainted-egotistic-ranting"
-db_host = "ubuunisrcdev01.cse.lehigh.edu"
-db_port = 5432
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
 
 try:
     # Test database connection
+    print(f"DB_HOST: {db_host}")
+    print(f"DB_PORT: {db_port}")
+    print(f"DB_NAME: {db_name}")
+    print(f"DB_USER: {db_user}")
+    print(f"DB_PASSWORD: {db_password}")
     connection = psycopg2.connect(
         dbname=db_name,
         user=db_user,
@@ -22,27 +28,34 @@ try:
         host=db_host,
         port=db_port
     )
+    
     print("Connection to the database was successful!")
 
 except Exception as e:
     print("Failed to connect to the database.")
     print(f"Error: {e}")
 
-finally:
-    if 'connection' in locals() and connection:
-        connection.close()
-        print("Connection closed.")
 
 app = Flask(__name__)
 @app.route("/courses")
 def courses():
     try:
-        # Query the database to retrieve student data
-        #result = db.session.execute(text("SELECT * FROM users"))
-        #students = result.fetchall()  # Fetch all rows
+        #Query the database to retrieve student data
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM my_schema.courses")
+        # Execute the SQL query to retrieve courses
+        courses = cursor.fetchall()  # Fetch all rows from the query
 
-        return render_template("courses.html")
+        for course in courses:
+            print(course)
+
+
+
+        return render_template("courses.html", courses=courses)
 
     except Exception as e:
         print(f"Error fetching student data: {e}")
         abort(500, description="Error fetching data from the database.")
+
+if __name__ == "__main__":
+    app.run(debug=True)
